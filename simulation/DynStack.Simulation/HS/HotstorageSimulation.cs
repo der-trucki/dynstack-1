55052420fc51002e846788cde1414f3c695a04c3
trucki <78615289+der-trucki@users.noreply.github.com>
2024-11-28 20:04:31 +0100

    old rewards

--- simulation/DynStack.Simulation/HS/HotstorageSimulation.cs
+++ simulation/DynStack.Simulation/HS/HotstorageSimulation.cs
@@ -584,6 +584,12 @@ namespace DynStack.Simulation.HS
         return;
       }
 
+      if (move.EmptyMove)
+      {
+        World.KPIs.CraneMoveReward = 0;
+        return;
+      }
+
       // Default is no reward
       World.KPIs.CraneMoveReward = 0;
       if (move.SourceId == 0) // Production -> X
@@ -591,7 +597,7 @@ namespace DynStack.Simulation.HS
         clearProd = true;
         if (World.Production.BottomToTop.Count == 0) // empty prod stack
         {
-          World.KPIs.CraneMoveReward += -1;
+          World.KPIs.CraneMoveReward += -0.25;
           clearProd = false;
         }
         else if (World.Production.BottomToTop.Count == 1)
@@ -600,7 +606,7 @@ namespace DynStack.Simulation.HS
         }
         else
         {
-          World.KPIs.CraneMoveReward += 1;
+          World.KPIs.CraneMoveReward += 0.25;
         }
       }
       else if (move.SourceId <= World.Buffers.Count &&
@@ -613,14 +619,14 @@ namespace DynStack.Simulation.HS
         {
           if (World.Buffers[sourceIndex].BottomToTop[i].Ready)
           {
-            World.KPIs.CraneMoveReward += 1;
+            World.KPIs.CraneMoveReward += 0.05;
           }
         }
 
         // if source gets empty with this move -> small reward
         if (World.Buffers[sourceIndex].BottomToTop.Count == 1)
         {
-          World.KPIs.CraneMoveReward += 1;
+          World.KPIs.CraneMoveReward += 0.15;
         }
       }
       else
@@ -652,12 +658,12 @@ namespace DynStack.Simulation.HS
           if (World.Buffers[targetIndex].BottomToTop.Count > 0 &&
               World.Buffers[targetIndex].BottomToTop[World.Buffers[targetIndex].BottomToTop.Count - 1].Ready)
           {
-            World.KPIs.CraneMoveReward += -1;
+            World.KPIs.CraneMoveReward += -0.5;
           }
           else if (readyBlock != null) // target stack contains min one ready block
           {
             // if source is prod, reward is "only" -25
-            World.KPIs.CraneMoveReward += -1;
+            World.KPIs.CraneMoveReward += -0.25;
           }
         }
       }
@@ -665,7 +671,7 @@ namespace DynStack.Simulation.HS
       { // X -> Handover
         if (!World.Handover.Ready || World.Handover.Block != null)
         {
-          World.KPIs.CraneMoveReward += -1;
+          World.KPIs.CraneMoveReward += -0.5;
         }
 
         Block block;
@@ -674,7 +680,7 @@ namespace DynStack.Simulation.HS
 
         if (block == null || !block.Ready)
         {
-          World.KPIs.CraneMoveReward += -1;
+          World.KPIs.CraneMoveReward += -0.5;
         }
         else
         {
